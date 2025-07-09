74e3fa94715b3ebb25ce03a02fba4b735ec76e00
ruszkiew <ed.ruszkiewicz>
2025-07-09 12:15:19 -0500

    added setup.py script

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup
 
 setup(
    name='sdwan',
-   version='1.0',
-   description='Cisco SD-WAN CLI Tool',
-   author='Ed Ruszkiewicz',
-   author_email='ed@ruszkiewicz.net',
-   packages=['sdwan'],
-   install_requires=['requests', 'pysocks', 'click', 'tabulate', 'netmiko']
+    version='7.8.0',
+    py_modules=['sdwan.py'],
+    install_requires=['requests', 'pysocks', 'click', 'tabulate', 'netmiko']
+    author = 'Ed Ruszkiewicz',
+    author_email = 'ed@ruszkiewicz.net',
+    description = 'Cisco SD-WAN CLI Tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ruszkiew/sdwan',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
